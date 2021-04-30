f6af6e2d69b165de22b6b048ad180428f907fec7
unknown <coco.30456@gmail.com>
2021-04-30 10:44:01 +0800

    hk3

---
+++ homework/03/Vector.js
@@ -0,0 +1,49 @@
+class Vector{
+    constructor(array){
+        this.a=array
+    }
+    add(y){
+        var r=[]
+        for(var i=0;i<x.a.length;i++)
+        {
+            r[i] = x.a[i]+y.a[i]
+        }
+        return new Vector(r)
+    }
+
+    sub(y){
+        var r=[]
+        for(var i=0;i<x.a.length;i++)
+        {
+            r[i] = x.a[i]-y.a[i]
+        }
+        return new Vector(r)
+    }
+
+    dot(y){
+        var r=[]
+        var n=0
+        for(var i=0;i<x.a.length;i++)
+        {
+            r[i] = x.a[i]*x.a[i]
+            n+=r[i]
+        }
+        return n
+    }
+
+    neg(y){
+        var r=[]
+        for(var i=0;i<x.a.length;i++)
+        {
+            r[i] = -x.a[i]
+        }
+        return new Vector(r)
+    }
+}
+
+var x=new Vector([1,2,3])
+var y=new Vector([1,1,1])
+console.log('x.add(y)=',x.add(y))
+console.log('x.sub(y)=',x.sub(y))
+console.log('x.dot(x)=',x.dot(x))
+console.log('x.neg(x)=',x.neg(x))
