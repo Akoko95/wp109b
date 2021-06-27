b038a347f9cb73384008f09363bd3bdced001327
Akoko95 <79677990+Akoko95@users.noreply.github.com>
2021-06-28 00:27:49 +0800

    Update nggpt.js

--- homework/MyWebPage/n-puzzle/js/nggpt.js
+++ homework/MyWebPage/n-puzzle/js/nggpt.js
@@ -4,10 +4,8 @@ $(function() {
 	for (var i = 3; i <= c1; i++) {
 		$("<option value='" + i + "'>" + i + "</option>").appendTo($("#n1,#n2"));
 	}
-	//選擇了正確的圖片之後，將其存入緩存之中，並且反映出效果
-	var imgurl = "img/smile.jpg"; //存圖片的base64格式的地址
+	var imgurl = "img/smile.jpg"; 
 	$("#img").change(function() {
-		//然后假如这条数据有效，那么久进入替换背景图和拼图原图，并且在此期间禁止操作
 		var file = $(this)[0].files[0];
 		if (file) {
 			$("#fimg").text(file.name);
@@ -23,8 +21,6 @@ $(function() {
 			})
 		}
 	})
-	//點擊並生成拼圖的大概流程
-	//生成亂序表，然後渲染正確的圖塊以幫助正確的方向事件
 	var x = 3;
 	var y = 3;
 	$("#btn").click(function() {
@@ -34,11 +30,11 @@ $(function() {
 	})
 
 	function drawgame() {
-		drawbox(); //將九宮格繪製到對應大小
-		var arr = getarr(); //獲得隨機數組順序
-		drawgrid(arr); //在九宮格中填充每個圖塊
+		drawbox(); 
+		var arr = getarr();
+		drawgrid(arr);
 	}
-	$(document).keydown(function(e) { //方向鍵控制相對應圖形移動
+	$(document).keydown(function(e) { 
 		var t2 = gett2();
 		if ((e.keyCode == 87 || e.keyCode == 38) && t2 <= (x - 1) * y) { //上
 			thechange(t2 + x, t2);
@@ -66,7 +62,7 @@ $(function() {
 			item.css(getpos(t1));
 		})
 	})
-	function iswin() { //判斷是否完成
+	function iswin() { 
 		var arr = $(".item").getarr();
 		var flag = true;
 		for(var i = 0;i<arr.length;i++){
@@ -81,12 +77,11 @@ $(function() {
 	}
 
 	function thechange(i1, i2) {
-		//將i1位置的拼圖移動到i2位置
 		the(i1).stop().animate(getpos(i2), 100);
 		the(i1).attr("data-t2", i2);
 	}
 
-	function gett2() { //取得空位置的t2
+	function gett2() { 
 		var t2 = -1;
 		for (var i = 1; i <= x * y; i++) {
 			if (the(i).length == 0) {
@@ -96,7 +91,6 @@ $(function() {
 		}
 		return t2;
 	}
-	//一個根據下標返回控制對象的方法
 	function the(e) {
 		return $(".item[data-t2='" + e + "']");
 	}
@@ -104,18 +98,18 @@ $(function() {
 	function drawgrid(arr) {
 		$("#box").find(".item").remove();
 		for (var i = 0; i < arr.length; i++) {
-			var t1 = arr[i]; //拼圖正確位置
-			var t2 = i + 1; //當前位置
+			var t1 = arr[i]; 
+			var t2 = i + 1; 
 			var $item = $("<div class='item' data-t1='" + t1 + "' data-t2='" + t2 + "'></div>");
 			$item.appendTo($("#box"));
-			$item.css(getpos(t2)) //根據當前位置將拼圖定位到正確的地方
-			$item.css(setpic(t1)) //這個圖塊裡面具體是哪一部分
+			$item.css(getpos(t2)) 
+			$item.css(setpic(t1)) 
 		}
 	}
 
 	function setpic(e) {
-		var x1 = e % x == 0 ? (x - 1) : e % x - 1; //取余知道纵坐标
-		var y1 = parseInt((e - 1) / x) //相除知道在第几行
+		var x1 = e % x == 0 ? (x - 1) : e % x - 1; 
+		var y1 = parseInt((e - 1) / x)
 		var temp = {
 			"background-position": "-" + x1 * 50 + "px -" + y1 * 50 + "px",
 			"background-image": " url(" + imgurl + ")",
@@ -125,8 +119,8 @@ $(function() {
 	}
 
 	function getpos(e) {
-		var x1 = e % x == 0 ? (x - 1) : e % x - 1; //取余知道纵坐标
-		var y1 = parseInt((e - 1) / x) //相除知道在第几行
+		var x1 = e % x == 0 ? (x - 1) : e % x - 1; 
+		var y1 = parseInt((e - 1) / x) 
 		return {
 			top: y1 * 50,
 			left: x1 * 50
@@ -169,10 +163,10 @@ $(function() {
 		return arr;
 	}
 
-	function drawbg() { //更改背景的
+	function drawbg() {
 		$("#bg").css("background-image", "url(" + imgurl + ")")
 	}
-	//一个将dom伪数组的对象转为jquerydom的数组对象
+	
 	$.prototype.getarr = function() {
 		var that = this;
 		var arr = [];
